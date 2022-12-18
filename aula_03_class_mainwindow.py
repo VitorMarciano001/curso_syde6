a8e1d43d68b6208d0d6b62a1b5d2096e0ccd1d96
VitorMarciano001 <vitor.marciano001@gmail.com>
2022-12-18 18:00:14 -0300

    Mandando arquivos

---
+++ aula_03_class_mainwindow.py
@@ -0,0 +1,28 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtCore import QSize
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Minha janela")
+        self.setFixedSize(QSize(600,400))
+
+        button = QPushButton("Botao 1")
+        self.setCentralWidget(button)
+
+        button.setCheckable(True)
+        button.clicked.connect(self.imprimir)
+        button.clicked.connect(self.clicado)
+
+    def imprimir(self):
+        print("Botao clicado")
+
+    def clicado(self, s):
+        print("clicado", s)
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec()
